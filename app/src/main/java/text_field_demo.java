2ddcea80add9cc7caded50edb0e75cb3d6d9845e
Wendell Bonto <wendell.bonto@gmail.com>
2018-10-03 19:58:32 +0800

    Wala nahuman

---
+++ app/src/main/java/text_field_demo.java
@@ -0,0 +1,11 @@
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.drawer.bonto.mobileprefilab.R;
+
+
+public class text_field_demo extends Fragment {
+
+
+}
---
+++ app/src/main/java/toggle_demo.java
@@ -0,0 +1,8 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.CompoundButton;
+
+public class toggle_demo extends Fragment {
+
+
+}
